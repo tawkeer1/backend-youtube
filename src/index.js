392ae51d134101fa7f4fde31fa5e4f6d45e3ee50
tawkeer1 <towqeerahmad111@gmail.com>
2024-03-07 22:37:05 +0530

    Error and response classes

--- src/index.js
+++ src/index.js
@@ -5,4 +5,11 @@ dotenv.config( {
     path: './env'
 })
 connectDB()
-
+.then(()=>{
+    app.listen(process.env.PORT || 4000,()=>{
+      console.log(`Server is running at PORT : ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MongoDB connection failed !!! ",err)
+})
